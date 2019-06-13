f36421ba3fd55f7fc19ce58dc0332bea55050173
Said LITIM <42178733+salade2doigts@users.noreply.github.com>
2019-06-13 22:06:15 +0200

    Optimisation

--- src/Form/Post1Type.php
+++ src/Form/Post1Type.php
@@ -3,27 +3,20 @@
 namespace App\Form;
 
 use App\Entity\Post;
-use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
-class PostType extends AbstractType
+class Post1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title')
-            ->add('category', EntityType::class,[
-                'class' => Category::class,
-                'choice_label' => 'title'
-            ])
-            ->add('content', TextareaType::class, [
-                'attr' => ['class' => 'mytextarea'],
-            ])
+            ->add('content')
             ->add('image')
+            ->add('createdAt')
+            ->add('category')
         ;
     }
 
